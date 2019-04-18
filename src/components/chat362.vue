42e3d2aae03dbded8c7c739e689341ae1d17dc83
peterx <peterx@iastate.edu>
2019-04-18 17:52:17 -0500

    new

--- src/components/chat362.vue
+++ src/components/chat362.vue
@@ -1,37 +1,65 @@
 <template>
   <div>
     <p>chat</p>
+    <button @click="send">send</button>
   </div>
 </template>
 <script>
     import axios from 'axios';
-    import VueSocketio from 'vue-socket.io';
-    // Vue.use(VueSocketio,'http://localhost:8181/gs-guide-websocket');
+    import SockJS from  'sockjs-client';
+    import  Stomp from 'stompjs';
     export default {
      
       data () {
         return {
           baseurl:"http://localhost:8181/",
-          id:666
+          websock: null,
+          stompClient:'',
 
         }
       },
 
       methods:{
-        
+        initWebSocket() {
+            this.connection();
+          
+        },  
+        connection() {
+            let that= this;
+           var socket = new SockJS('http://localhost:8181/ws');
+            this.stompClient = Stomp.over(socket);
+            this.stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+              that.stompClient.subscribe('/topic/hello',(msg)=>{
+              console.log("hello success",msg);
+            })
+          });
+        },   
+       disconnect() {
+            if (this.stompClient !== null) {
+                this.stompClient.disconnect();
+            }
+            console.log("Disconnected");
+        },
+
+
+
+       send() {
+        console.log("send message");
+            this.stompClient.send("/app/hello", {}, "hi this is web");
+        }
+
+
+
+
       },
 
       mounted(){  
-
+        this.initWebSocket();
       }, 
-      sockets:{
-        // connect:function(){
-        //   this.id=this.$sockets.id;
-        //   console.log("connect",this.id);
-        // },
-        // new_msg: function(msg){           
-        //     console.log(msg);
-        // }
+      beforeDestroy: function () {
+        // 页面离开时断开连接,清除定时器
+        // this.disconnect();
       }
     }
 </script>
